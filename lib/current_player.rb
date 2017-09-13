a5135de1b22e768e2be82eca3005c3f4db258ff6
Nicholas Halloran <nmhalloran@hotmail.com>
2017-09-13 21:20:23 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,5 @@
 def turn_count(board)
-  turn = 0
+  turn = 1
   board.each do |cell|
     if cell == "X" || cell == "O"
       turn += 1
@@ -9,7 +9,7 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board) % 2 == 1
+  if turn_count(board) % 2 == 0
     player = "X"
   else
     player = "O"
